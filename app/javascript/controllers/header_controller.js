060fcab3445a3d90e956a4ff7b9c5affde10a8b3
HMADAHMD <hammadja01@gmail.com>
2024-09-01 14:35:45 +0500

    feature: add modal to show and hide and fix drop down close

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,13 +1,31 @@
-import { Controller } from "@hotwired/stimulus"
-import {enter, leave, toggle} from 'el-transition'
+import { Controller } from "@hotwired/stimulus";
+import { enter, leave, toggle } from "el-transition";
 
 export default class extends Controller {
-  static targets = ['openUserMenu'];
+  static targets = ['openUserMenu', 'userAuthLink'];
   connect() {
     this.openUserMenuTarget.addEventListener('click', this.toggleDropDownMenu);
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById("modal-trigger").click();
+      })
+    });
+    
+    document.addEventListener('click', this.handleOutsideClick.bind(this));
   }
 
-  toggleDropDownMenu() {
+  toggleDropDownMenu(event) {
+    event.stopPropagation();
     toggle(document.getElementById('menu-dropdown-items'));
+  }
+
+  handleOutsideClick(event) {
+    const dropdownMenu = document.getElementById('menu-dropdown-items');
+    const isOpen = !dropdownMenu.classList.contains('hidden');
+    
+    if (isOpen && !this.openUserMenuTarget.contains(event.target) && !dropdownMenu.contains(event.target)) {
+      leave(dropdownMenu);
+    }
   }
 }
